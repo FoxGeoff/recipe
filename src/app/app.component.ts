44219a7f74e9863bcbbf3982ce3e255afce58932
Geoff Fox <fox.geoffrey@gmail.com>
2021-06-13 18:36:37 -0700

    readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,7 @@ export class AppComponent {
     console.log(form);
   } */
 
-  onSubmit(form: HTMLFormElement) {
+  onSubmit(form: NgForm) {
     console.log(form);
   }
 }
